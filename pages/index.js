deccfa2dbd96e5c217dbbf1273f8c15fe351f03e
Edward Kabue <ekabue0@gmail.com>
2021-12-17 05:19:48 +0300

    created hiden scrollbar nav

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -10,9 +11,10 @@ export default function Home() {
         <meta name="description" content="Huluu" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       {/*Header*/}
       <Header />
+      {/*Nav*/}
+      <Nav />
     </div>
   );
 }
