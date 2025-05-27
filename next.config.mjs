5994ad7b35b1a0a601c999f0406b4c8261fbeba7
Edward-Kabue <ekabue0@gmail.com>
2025-05-27 13:07:20 +0300

    Refactor code structure for improved readability and maintainability

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['press.hulu.com', 'image.tmdb.org'],
+  },
+};
+
+export default nextConfig;
+
--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,23 +1,25 @@
-import {
-  BadgeCheckIcon,
-  CollectionIcon,
-  HomeIcon,
-  LightningBoltIcon,
-  SearchIcon,
-  UserIcon,
-} from "@heroicons/react/outline";
+"use client";
+
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BoltIcon,
+  CheckBadgeIcon,
+  RectangleStackIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
 function Header() {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
-        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
-        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
-        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="TRENDING" Icon={BoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={CheckBadgeIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={RectangleStackIcon} />
+        <HeaderItem title="SEARCH" Icon={MagnifyingGlassIcon} />
         <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image
@@ -25,6 +27,7 @@ function Header() {
         src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
         width={200}
         height={100}
+        alt="Hulu Logo"
       />
     </header>
   );
